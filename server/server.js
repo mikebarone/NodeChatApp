3adf1c0baad15e2cf08a0e266f0ada6b734538f1
mikebarone <mikebarone@gmail.com>
2017-04-24 16:21:54 -0400

    Add Sockets

--- server/server.js
+++ server/server.js
@@ -1,16 +1,27 @@
 require('./config/config.js');
-var express = require('express');
+const http = require('http');
+const express = require('express');
 const path = require('path');
+const socketIO = require('socket.io');
 
 const publicPath = path.join(__dirname, '../public');
 const port = process.env.PORT;
 
 var app = express();
+var server = http.createServer(app);
+var io = socketIO(server);
 
 // Public folder for site content
 app.use(express.static(publicPath));
 
-app.listen(port, () => {
+io.on('connection', (socket) => {
+    console.log('New User Connected');
+    socket.on('disconnect', () => {
+        console.log('User was Disconnected');
+    });
+});
+
+server.listen(port, () => {
     console.log(`Started up on port ${port}`);
 });
 
